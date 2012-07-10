00792fd7aaab833266359c03084af779a19856f5
Takayuki Miyauchi <miya@theta.ne.jp>
2012-07-11 02:38:30 +0900

    first commit

---
+++ oembed-instagram.php
@@ -0,0 +1,100 @@
+<?php
+/*
+Plugin Name: oEmbed Instagram
+Plugin URI: http://wpist.me/wp/oembed-instagram
+Description: Embed source from instagram.
+Author: Takayuki Miyauchi
+Version: 1.0.0
+Author URI: http://wpist.me/
+*/
+
+new oEmbedInstagram();
+
+class oEmbedInstagram {
+
+private $api = "http://api.instagram.com/oembed?url=%s&maxwidth=%d";
+private $large_img = "%s/media/?size=l";
+private $prefix = '_instagram_';
+
+function __construct()
+{
+    add_action('plugins_loaded', array(&$this, 'plugins_loaded'));
+}
+
+public function plugins_loaded()
+{
+    wp_embed_register_handler(
+        'instagram',
+        '#http://instagram.com/p/[a-zA-Z0-9]+/?$#i',
+        array(&$this, 'oembed_handler')
+    );
+}
+
+public function oembed_handler($m, $attr, $url, $rattr)
+{
+    $url = preg_replace("#/$#", "", $m[0]);
+
+    $post_id = get_the_id();
+    if ($meta = get_post_meta($post_id, $this->prefix.$url)) {
+        $data = $meta[0];
+    } else {
+        $data = $this->get_from_api($url);
+    }
+
+    $title = $data->title;
+    $image = $data->url;
+    $large = sprintf($this->large_img, $url);
+    $width = $data->width;
+    $height = $data->height;
+
+    return sprintf(
+        $this->get_template(),
+        $width+10,
+        $large,
+        $title,
+        $image,
+        $width,
+        $height,
+        $title
+    );
+}
+
+private function get_from_api($url)
+{
+    global $content_width;
+
+    $max_width = 0;
+    if (isset($content_width)) {
+        $max_width = $content_width;
+    }
+
+    $api = sprintf(
+        $this->api,
+        $url,
+        $max_width
+    );
+
+    $res = wp_remote_get($api);
+    if ($res['response']['code'] === 200) {
+        $json = json_decode($res['body']);
+        $post_id = get_the_id();
+        update_post_meta($post_id, $this->prefix.$url, $json);
+        return $json;
+    }
+}
+
+private function get_template()
+{
+    $html =<<<EOL
+<div class="wp-caption alignleft oembed-instagram" style="width:%dpx">
+    <a href="%s"><img class="size-full" title="%s" src="%s" alt="" width="%d" height="%d" /></a>
+    <p class="wp-caption-text">%s</p>
+</div>
+EOL;
+
+    return $html;
+}
+
+}
+
+// EOF
---
+++ readme.txt
@@ -0,0 +1,49 @@
+=== oEmbed Instagram ===
+Contributors: miyauchi
+Donate link: https://www.paypal.com/jp/cgi-bin/webscr?cmd=_flow&SESSION=V9ggRQYdPscPXHHzwIMAG2o2RavMQO8KLdFTxHm3dzuX2gDiDHw-k1ZBw5C&dispatch=50a222a57771920b6a3d7b606239e4d529b525e0b7e69bf0224adecfb0124e9b61f737ba21b0819838956b846fa597911f234fb937456ea7
+Tags: url, link, oembed, visual editor, post, instagram
+Requires at least: 3.4
+Tested up to: 3.4.1
+Stable tag: 1.0.0
+
+Embed your photo from instagram into WordPress easily.
+
+== Description ==
+
+* Embed your photo from instagram into WordPress easily.
+* You simply paste URL of your instagram photo.
+
+= Summary =
+
+You can embed your photo from instagram simply paste URL like below.
+
+* http://instagram.com/p/MtBjw1iIx4/
+
+PHP5 is required!
+
+= Support =
+
+* [Plugin Homepage](http://wpist.me/wp/oembed-instagram)
+* [Support](http://wordpress.org/tags/oembed-instagram)
+
+
+== Installation ==
+
+* A plug-in installation screen is displayed on the WordPress admin panel.
+* It installs it in `wp-content/plugins`.
+* The plug-in is made effective.
+
+
+== Changelog ==
+
+= 1.0.0 =
+* The first release.
+
+== Credits ==
+
+This plug-in is not guaranteed though the user of WordPress can freely use this plug-in free of charge regardless of the purpose.
+The author must acknowledge the thing that the operation guarantee and the support in this plug-in use are not done at all beforehand.
+
+== Contact ==
+
+* twitter @wpist_me
